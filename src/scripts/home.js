d70c5d395494888d6511355ce6ef65df5afe9d1b
mstrlaw <lawbraun.almeida@gmail.com>
2021-12-12 02:30:12 +0000

    Improve homepage script to load chart

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,40 +1,13 @@
-const data = [];
-const labels = [];
-
-const injectChartist = () => new Promise((resolve, reject) => {
-  // Add chart script
+const injectScript = (url) => new Promise((resolve, reject) => {
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.async = true;
-  script.src = '//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js';
-
+  script.src = url;
   // Append the script to the DOM
   const el = document.getElementsByTagName('script')[0]
   el.parentNode.insertBefore(script, el)
-
   // Resolve the promise once the script is loaded
-  script.addEventListener('load', () => {
-    console.log('chart script loaded');
-    resolve();
-  });
-});
-
-const injectPlugin = () => new Promise((resolve, reject) => {
-  // Add chart script
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.src = '//cdn.jsdelivr.net/npm/chartist-plugin-pointlabels@0.0.6/dist/chartist-plugin-pointlabels.min.js';
-
-  // Append the script to the DOM
-  const el = document.getElementsByTagName('script')[0]
-  el.parentNode.insertBefore(script, el)
-
-  // Resolve the promise once the script is loaded
-  script.addEventListener('load', () => {
-    console.log('chart plugin loaded');
-    resolve();
-  });
+  script.addEventListener('load', () => resolve());
 });
 
 const loadChartData = async () =>
@@ -45,110 +18,58 @@ const loadChartData = async () =>
       }
     });
 
-const getTimeLabel = (date, seconds) => {
-  const endDate = new Date(date);
+const getTimeLabel = (seconds) => {
+  const endDate = new Date();
   endDate.setSeconds(seconds);
   return `${endDate.getHours()}h ${endDate.getMinutes()}m`;
 };
 
-const formatLabel = (date, seconds) => {
-  const dayNumber = date.getDay();
-  const timeLabel = getTimeLabel(date, seconds);
-  let dayString = '';
-
-  switch(dayNumber){
-    case 0:
-        dayString = 'Sun';
-        break;
-    case 1:
-        dayString = 'Mon';
-        break;
-    case 2:
-        dayString = 'Tue';
-        break;
-    case 3:
-        dayString = 'Wed';
-        break;
-    case 4:
-        dayString = 'Thu';
-        break;
-    case 5:
-        dayString = 'Fri';
-        break;
-    case 6:
-        dayString = 'Sat';
-        break;
-  }
-
-  return `${dayString}. ${timeLabel}`;
-}
-
-window.onload = async () => { // can also use window.addEventListener('load', (event) => {
-  console.log('Page loaded');
-
-  await injectChartist();
-  await injectPlugin();
-
-
-  // // Async load chartisjs plugin
-  // const script = document.querySelector('#chartistjs');
-  // console.log(script);
-
-  // script.addEventListener('load', function() {
-  //   console.log('chartist loaded');
-  // });
-
-  let totalTime = 0;
-  const tmp = [];
-  const dataRequest = loadChartData();
-
-  dataRequest.then(res => {
-    if (res.data.data.length === 0) return;
-    res.data.data.map( d => {
-      const date = new Date(d.range.start);
-      tmp.push({
-        date,
-        formatted: formatLabel(date, d.grand_total.total_seconds),
-        data: d.grand_total.total_seconds,
-      });
-
-      totalTime += d.grand_total.total_seconds;
-    });
-    console.log(tmp);
-  })
-  .catch(err => {
-    console.log(err);
-    console.log('error fetching chart data');
-  });
-
+const drawChart = (series) => {
   var data = {
-    // A labels array that can contain any sort of values
     labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
     // Our series array that contains series objects or in this case series data arrays
     series: [
-      [ 5.1234, 2.4121, 4.3131, 2.222, 1.12312 ]
+      series,
     ]
   };
-
+  const axisMod = {
+    showGrid: false,
+    showLabel: false,
+    offset: 0,
+  };
   const options = {
-    // showPoint: false,
     fullWidth: true,
-    axisX: {
-      showGrid: false,
-      showLabel: false,
-      offset: 0,
-    },
-    axisY: {
-      showGrid: false,
-      showLabel: false,
-      offset: 0
-    },
+    chartPadding: { top: 0, right: 25, left: 25, bottom: 0 },
+    axisX: {...axisMod},
+    axisY: {...axisMod},
     plugins: [
       Chartist.plugins.ctPointLabels({
-        textAnchor: 'middle',
-        labelInterpolationFnc: (value) => '$' + value.toFixed(2)
-      })
+        labelInterpolationFnc: (value) => getTimeLabel(value)
+      }),
     ],
   }
   new Chartist.Line('.ct-chart', data, options);
+};
+
+window.onload = async () => {
+  await injectScript('//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js');
+  await injectScript('//cdn.jsdelivr.net/npm/chartist-plugin-pointlabels@0.0.6/dist/chartist-plugin-pointlabels.min.js');
+
+  const dataRequest = loadChartData();
+  let series = [];
+  let totalTime = 0;
+
+  dataRequest
+    .then(res => {
+      if (res.data.data.length === 0) return;
+      res.data.data.forEach( d => {
+        series.push(d.grand_total.total_seconds);
+        totalTime += d.grand_total.total_seconds;
+      });
+      drawChart(series);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log('error fetching chart data');
+    });
 };
