968ee19ced0efc8ff0e8d49ff2e900bf0ac2c6db
Lawrence <lawbraun.almeida@gmail.com>
2025-01-29 23:01:29 +0000

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  output: 'static',
   integrations: [vue()],
+  adapter: netlify(),
 });
