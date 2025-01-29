8d67bc28ea6f842d65fc98b3545b6afbf07e89c0
Lawrence <lawbraun.almeida@gmail.com>
2025-01-29 12:07:29 +0000

    Stable work in progress

---
+++ src/components/FeaturedMedia.vue
@@ -0,0 +1,217 @@
+<script setup>
+// import { ref } from 'vue';
+
+// let visibility = ref(false);
+
+// const toggleVisibility = () => {
+//   visibility.value = !visibility.value;
+// };
+</script>
+
+<template>
+  hello
+  <!-- <vue-horizontal>
+    <div class="item">
+      <h3>HTML Tag</h3>
+      <p>As you can see these are just html elements.</p>
+    </div>
+    <section>
+      <h4>Don't have to be the same tag</h4>
+      <p>I used a h4 instead of a h3</p>
+    </section>
+    <section>
+      <h3>Navigation Button</h3>
+      <p>The navigation button will appear if there is an overflow.</p>
+    </section>
+    <section>
+      <h3>Scroll</h3>
+      <p>You can just trackpad to scroll still!</p>
+    </section>
+    <section>
+      <h3>Touch screen</h3>
+      <p>Touch screen works too!</p>
+    </section>
+    <section v-for="item in items" :key="item.i">
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.content }}</p>
+    </section>
+    <section>
+      <h3>Last item?</h3>
+      <p>Maybe you want to display something different at the end?</p>
+    </section>
+  </vue-horizontal> -->
+  <div class="mt-16 sm:mt-20">
+    <div class="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+      <div
+        class="relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2"
+      >
+        <img
+          alt=""
+          loading="lazy"
+          width="3744"
+          height="5616"
+          decoding="async"
+          data-nimg="1"
+          class="absolute inset-0 h-full w-full object-cover"
+          sizes="(min-width: 640px) 18rem, 11rem"
+          srcset="
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=16&amp;q=75     16w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=32&amp;q=75     32w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=48&amp;q=75     48w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=64&amp;q=75     64w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=96&amp;q=75     96w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=128&amp;q=75   128w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=256&amp;q=75   256w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=384&amp;q=75   384w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=640&amp;q=75   640w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=750&amp;q=75   750w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=828&amp;q=75   828w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=1080&amp;q=75 1080w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=1200&amp;q=75 1200w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=1920&amp;q=75 1920w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=2048&amp;q=75 2048w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=3840&amp;q=75 3840w
+          "
+          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.c5d2141c.jpg&amp;w=3840&amp;q=75"
+          style="color: transparent"
+        />
+      </div>
+      <div
+        class="relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 -rotate-2"
+      >
+        <img
+          alt=""
+          loading="lazy"
+          width="3936"
+          height="2624"
+          decoding="async"
+          data-nimg="1"
+          class="absolute inset-0 h-full w-full object-cover"
+          sizes="(min-width: 640px) 18rem, 11rem"
+          srcset="
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=16&amp;q=75     16w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=32&amp;q=75     32w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=48&amp;q=75     48w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=64&amp;q=75     64w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=96&amp;q=75     96w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=128&amp;q=75   128w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=256&amp;q=75   256w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=384&amp;q=75   384w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=640&amp;q=75   640w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=750&amp;q=75   750w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=828&amp;q=75   828w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=1080&amp;q=75 1080w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=1200&amp;q=75 1200w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=1920&amp;q=75 1920w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=2048&amp;q=75 2048w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=3840&amp;q=75 3840w
+          "
+          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-2.3c6c01cf.jpg&amp;w=3840&amp;q=75"
+          style="color: transparent"
+        />
+      </div>
+      <div
+        class="relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2"
+      >
+        <img
+          alt=""
+          loading="lazy"
+          width="5760"
+          height="3840"
+          decoding="async"
+          data-nimg="1"
+          class="absolute inset-0 h-full w-full object-cover"
+          sizes="(min-width: 640px) 18rem, 11rem"
+          srcset="
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=16&amp;q=75     16w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=32&amp;q=75     32w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=48&amp;q=75     48w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=64&amp;q=75     64w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=96&amp;q=75     96w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=128&amp;q=75   128w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=256&amp;q=75   256w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=384&amp;q=75   384w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=640&amp;q=75   640w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=750&amp;q=75   750w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=828&amp;q=75   828w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=1080&amp;q=75 1080w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=1200&amp;q=75 1200w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=1920&amp;q=75 1920w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=2048&amp;q=75 2048w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=3840&amp;q=75 3840w
+          "
+          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-3.454151b1.jpg&amp;w=3840&amp;q=75"
+          style="color: transparent"
+        />
+      </div>
+      <div
+        class="relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2"
+      >
+        <img
+          alt=""
+          loading="lazy"
+          width="2400"
+          height="3000"
+          decoding="async"
+          data-nimg="1"
+          class="absolute inset-0 h-full w-full object-cover"
+          sizes="(min-width: 640px) 18rem, 11rem"
+          srcset="
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=16&amp;q=75     16w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=32&amp;q=75     32w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=48&amp;q=75     48w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=64&amp;q=75     64w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=96&amp;q=75     96w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=128&amp;q=75   128w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=256&amp;q=75   256w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=384&amp;q=75   384w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=640&amp;q=75   640w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=750&amp;q=75   750w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=828&amp;q=75   828w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=1080&amp;q=75 1080w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=1200&amp;q=75 1200w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=1920&amp;q=75 1920w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=2048&amp;q=75 2048w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=3840&amp;q=75 3840w
+          "
+          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&amp;w=3840&amp;q=75"
+          style="color: transparent"
+        />
+      </div>
+      <div
+        class="relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 -rotate-2"
+      >
+        <img
+          alt=""
+          loading="lazy"
+          width="4240"
+          height="2384"
+          decoding="async"
+          data-nimg="1"
+          class="absolute inset-0 h-full w-full object-cover"
+          sizes="(min-width: 640px) 18rem, 11rem"
+          srcset="
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=16&amp;q=75     16w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=32&amp;q=75     32w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=48&amp;q=75     48w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=64&amp;q=75     64w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=96&amp;q=75     96w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=128&amp;q=75   128w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=256&amp;q=75   256w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=384&amp;q=75   384w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=640&amp;q=75   640w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=750&amp;q=75   750w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=828&amp;q=75   828w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=1080&amp;q=75 1080w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=1200&amp;q=75 1200w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=1920&amp;q=75 1920w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=2048&amp;q=75 2048w,
+            /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=3840&amp;q=75 3840w
+          "
+          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.6c6f2784.jpg&amp;w=3840&amp;q=75"
+          style="color: transparent"
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,192 @@
+<script setup>
+import { ref } from 'vue'
+
+let visibility = ref(false)
+
+const toggleVisibility = () => {
+  visibility.value = !visibility.value
+}
+</script>
+
+<template>
+  <header class="pointer-events-none relative z-50 flex flex-none flex-col">
+    <div class="top-0 z-10 h-16 pt-6">
+      <div
+        class="sm:px-8 top-(--header-top,--spacing(6)) w-full"
+      >
+        <div class="mx-auto w-full max-w-7xl lg:px-8">
+          <div class="relative px-8 lg:px-12">
+            <div class="mx-auto max-w-2xl lg:max-w-5xl">
+              <div class="relative flex gap-4">
+                <!-- <div class="flex flex-1"></div> -->
+                <div class="flex flex-1 justify-end md:justify-center">
+                  <div class="pointer-events-auto md:hidden">
+                    <button
+                      class="group flex items-center h-full rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
+                      type="button"
+                      aria-expanded="false"
+                      data-headlessui-state=""
+                      @click="toggleVisibility"
+                    >
+                      Menu
+                      <svg
+                        viewBox="0 0 8 6"
+                        aria-hidden="true"
+                        class="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400"
+                      >
+                        <path
+                          d="M1.75 1.75 4 4.25l2.25-2.5"
+                          fill="none"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"></path>
+                      </svg>
+                    </button>
+                    <div
+                      v-if="visibility"
+                      class="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-xs duration-150 data-closed:opacity-0 data-enter:ease-out data-leave:ease-in dark:bg-black/80"
+                      id="headlessui-popover-backdrop-:Rjmiqja:"
+                      aria-hidden="true"
+                      style=""
+                    >
+                    </div>
+                    <div
+                      v-if="visibility"
+                      class="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 duration-150 data-closed:scale-95 data-closed:opacity-0 data-enter:ease-out data-leave:ease-in dark:bg-zinc-900 dark:ring-zinc-800"
+                      id="headlessui-popover-panel-:Rrmiqja:"
+                      tabindex="-1"
+                    >
+                      <div
+                        v-if="visibility"
+                        class="flex flex-row-reverse items-center justify-between"
+                      >
+                        <button
+                          aria-label="Close menu"
+                          class="-m-1 p-1"
+                          type="button"
+                          data-headlessui-state="open active"
+                          data-open=""
+                          data-active=""
+                          @click="toggleVisibility"
+                          ><svg
+                            viewBox="0 0 24 24"
+                            aria-hidden="true"
+                            class="h-6 w-6 text-zinc-500 dark:text-zinc-400"
+                            ><path
+                              d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"></path></svg
+                          ></button
+                        ><h2
+                          class="text-sm font-medium text-zinc-600 dark:text-zinc-400"
+                        >
+                          mstrlaw
+                        </h2>
+                      </div>
+                      <nav class="mt-6">
+                        <ul
+                          class="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300"
+                        >
+                          <li>
+                            <a
+                              class="block py-2"
+                              data-headlessui-state="open active"
+                              data-open=""
+                              data-active=""
+                              href="/writing">Writing</a
+                            >
+                          </li><li>
+                            <a
+                              class="block py-2"
+                              data-headlessui-state="open active"
+                              data-open=""
+                              data-active=""
+                              href="/projects">Projects</a
+                            >
+                          </li>
+                          <li>
+                            <a
+                              class="block py-2"
+                              data-headlessui-state="open active"
+                              data-open=""
+                              data-active=""
+                              href="/uses">Uses</a
+                            >
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
+                  </div>
+                  <nav class="pointer-events-auto hidden md:block">
+                    <ul
+                      class="flex items-center rounded-full h-full bg-white/90 px-3 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+                    >
+                      <li>
+                        <a
+                          class="relative block px-3 py-2 transition hover:text-indigo-800 dark:hover:text-indigo-300"
+                          href="/writing">Writing</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          class="relative block px-3 py-2 transition hover:text-indigo-800 dark:hover:text-indigo-300"
+                          href="/projects">Projects</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          class="relative block px-3 py-2 transition hover:text-indigo-800 dark:hover:text-indigo-300"
+                          href="/values">Values</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          class="relative block px-3 py-2 transition hover:text-indigo-800 dark:hover:text-indigo-300"
+                          href="/uses">Uses</a
+                        >
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div v-if="false" class="flex justify-end md:flex-1">
+                  <div class="pointer-events-auto">
+                    <button
+                      type="button"
+                      aria-label="Switch to light theme"
+                      class="group rounded-full bg-white/90 px-3 py-2 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+                      ><svg
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        aria-hidden="true"
+                        class="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-indigo-50 [@media(prefers-color-scheme:dark)]:stroke-indigo-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-indigo-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-indigo-600"
+                        ><path
+                          d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"
+                        ></path><path
+                          d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
+                          fill="none"></path></svg
+                      ><svg
+                        viewBox="0 0 24 24"
+                        aria-hidden="true"
+                        class="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media_not_(prefers-color-scheme:dark)]:fill-indigo-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-indigo-500 [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400"
+                        ><path
+                          d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"></path></svg
+                      ></button
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
