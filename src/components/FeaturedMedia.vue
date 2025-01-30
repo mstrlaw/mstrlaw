9721f750ead5db4c14df8b93ec7e4fef541c716c
Lawrence <lawbraun.almeida@gmail.com>
2025-01-30 01:00:09 +0000

    Add click event to navigate to featured image

--- src/components/FeaturedMedia.vue
+++ src/components/FeaturedMedia.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from 'vue';
 import 'vue3-carousel/carousel.css';
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import { Carousel, Slide } from 'vue3-carousel';
 import { Image } from '@unpic/vue';
+
+const activeSlide = ref(0);
 
 const images = [
   {
@@ -67,20 +70,24 @@ const carouselConfig = {
     },
     992: {
       itemsToShow: 5,
-      snapAlign: 'start',
+      snapAlign: 'center',
       gap: 300,
     },
     1100: {
       itemsToShow: 5,
-      snapAlign: 'start',
+      snapAlign: 'center',
       gap: 300,
     },
     1400: {
       itemsToShow: 5,
-      snapAlign: 'start',
+      snapAlign: 'center',
       gap: 300,
     },
   },
+};
+
+const goToSlide = (index) => {
+  activeSlide.value = index;
 };
 </script>
 
@@ -88,8 +95,12 @@ const carouselConfig = {
   <div class="my-16 w-full md:max-w-7xl mx-auto">
     <div class="overflow-hidden py-4 sm:gap-8">
       <!-- <div class="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8"> -->
-      <Carousel v-bind="carouselConfig">
-        <Slide v-for="(image, index) in images" :key="index">
+      <Carousel v-model="activeSlide" v-bind="carouselConfig">
+        <Slide
+          v-for="(image, index) in images"
+          :key="index"
+          @click="goToSlide(index)"
+        >
           <!-- <div class="carousel__item">{{ slide }}</div> -->
           <div
             class="relative aspect-9/10 w-64 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2 my-4 hover:cursor-grab active:cursor-grabbing"
@@ -104,6 +115,7 @@ const carouselConfig = {
               :alt="image.alt"
               decoding="async"
               class="absolute inset-0 h-full w-full object-cover"
+              fallback="netlify"
             />
             <!-- <img
               alt=""
