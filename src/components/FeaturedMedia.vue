78ae766b35a748f05dda89224a3e3a4a5f30bc25
Lawrence <lawbraun.almeida@gmail.com>
2025-01-30 00:24:22 +0000

    Add all featured images

--- src/components/FeaturedMedia.vue
+++ src/components/FeaturedMedia.vue
@@ -3,6 +3,57 @@ import 'vue3-carousel/carousel.css';
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 import { Image } from '@unpic/vue';
 
+const images = [
+  {
+    name: 'a',
+    alt: 'Two roped men climbing up a steeped bank of snow.',
+  },
+  {
+    name: 'b',
+    alt: 'A group of people marching on snow with tall white mountains in the background.',
+  },
+  {
+    name: 'c',
+    alt: 'A white truck in an orange tinted desert.',
+  },
+  {
+    name: 'd',
+    alt: 'A palm tree and an egyptian obelisc in front of a blue and pink sky.',
+  },
+  {
+    name: 'e',
+    alt: 'Walking down a wooden pathway with a surfboard towards the ocean.',
+  },
+  {
+    name: 'f',
+    alt: 'A girl looking back while skating on a longboard surrounded by palm trees.',
+  },
+  {
+    name: 'g',
+    alt: 'A girl riding a colorful Google bike while laughing at the camera.',
+  },
+  {
+    name: 'h',
+    alt: 'The Atkas statue in Rockefeller Center in New York.',
+  },
+  {
+    name: 'i',
+    alt: 'A woman flying a Cesna plane with a man as the passenger looking out the window.',
+  },
+  {
+    name: 'j',
+    alt: 'A man standing on top of a snowy mountain peak holding an ice axe.',
+  },
+  {
+    name: 'k',
+    alt: 'A man looking facing the sun set horizon with a dark orange rock wall behind him.',
+  },
+  {
+    name: 'l',
+    alt: 'A man in a wetsuit holding a surfboard and laughing at the beach.',
+  },
+];
+
 const carouselConfig = {
   itemsToShow: 2,
   wrapAround: true,
@@ -38,19 +89,19 @@ const carouselConfig = {
     <div class="overflow-hidden py-4 sm:gap-8">
       <!-- <div class="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8"> -->
       <Carousel v-bind="carouselConfig">
-        <Slide v-for="slide in 10" :key="slide">
+        <Slide v-for="(image, index) in images" :key="index">
           <!-- <div class="carousel__item">{{ slide }}</div> -->
           <div
             class="relative aspect-9/10 w-64 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 rotate-2 my-4 hover:cursor-grab active:cursor-grabbing"
           >
             <Image
-              src="/photos/a.jpeg"
+              :src="`/photos/${image.name}.jpeg`"
               layout="constrained"
               loading="lazy"
               background="auto"
               width="800"
               height="1066"
-              alt="A lovely bath"
+              :alt="image.alt"
               decoding="async"
               class="absolute inset-0 h-full w-full object-cover"
               fallback="netlify"
