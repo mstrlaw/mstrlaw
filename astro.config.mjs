46bc7423099a56c9ac0940b5ff0eb440fe4a3225
Lawrence <lawbraun.almeida@gmail.com>
2025-02-01 15:14:59 +0000

    Setup vite alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@astrojs/vue";
-import netlify from "@astrojs/netlify";
+
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      /* Vite aliases */
+      alias: {
+        '@': '/src',
+      },
+  },
   },
   output: 'static',
   integrations: [vue()],
