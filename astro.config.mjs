335aad7d832dd5a7aa35ed00ae5f915d5a9a7233
Lawrence <lawbraun.almeida@gmail.com>
2025-02-09 17:26:44 +0000

    Adds purgecss to remove unused CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { imageService } from "@unpic/astro/service";
-
-
-
+import { imageService } from '@unpic/astro/service';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
 import netlify from '@astrojs/netlify';
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   output: 'static',
-  integrations: [vue()],
+  integrations: [vue(), purgecss()],
   adapter: netlify(),
   image: {
     service: imageService(),
