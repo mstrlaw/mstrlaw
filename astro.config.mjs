5f38947fa770c51b1a24eb2e9bccb19be7c5e7ec
Lawrence <lawbraun.almeida@gmail.com>
2025-11-09 15:38:28 +0000

    Remove astro netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { imageService } from '@unpic/astro/service';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@astrojs/vue';
-import netlify from '@astrojs/netlify';
-import { templateCompilerOptions } from '@tresjs/core';
+import { defineConfig } from 'astro/config'
+import { imageService } from '@unpic/astro/service'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@astrojs/vue'
+import { templateCompilerOptions } from '@tresjs/core'
 
-import compressor from 'astro-compressor';
+import compressor from 'astro-compressor'
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,8 +19,7 @@ export default defineConfig({
   },
   output: 'static',
   integrations: [vue(), compressor()],
-  adapter: netlify(),
   image: {
     service: imageService(),
   },
-});
+})
